f66a78813764408584711526443052f1fad8c3b2
dairenalxcomscience <enaldaingah4@gmail.com>
2026-02-27 10:06:02 +0900

    pusing to varcel

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Sesuai dengan nama repo di 'homepage' package.json Anda
-  base: "/testingIsd/", 
+  base: "/", 
 })
